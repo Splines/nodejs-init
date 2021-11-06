f9528c0ed31bcd3ba2cecd346d215c9d46963a67
Splines <dominic-plein@gmx.de>
2021-11-06 22:33:16 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Find out more here:
+// https://khalilstemmler.com/blogs/typescript/node-starter-project/
+
+// If you rename this file:
+// Rename in nodemon.json as well as in the package.json scripts.
+
+console.log('Application successfully started')
